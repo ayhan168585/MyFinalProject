6580199661656f31ae4e2e3b3d63174c50cbed5c
ayhan168585 <ayhan168585@hotmail.com>
2024-11-24 02:53:54 +0300

    Core katmanı oluşturuldu.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -27,5 +27,11 @@ namespace Business.Concrete
         {
             return _productDal.GetAll(p=>p.CategoryId==id);
         }
+
+        public List<Product> GetAllByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p=>p.UnitPrice>=min && p.UnitPrice<=max);
+
+        }
     }
 }
