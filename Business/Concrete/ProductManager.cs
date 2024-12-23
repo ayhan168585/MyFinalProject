b7d8cc0996ac86f22890ed956ab5f18ad8c6f7f4
ayhan168585 <ayhan168585@hotmail.com>
2024-12-24 02:22:25 +0300

    cachleme işlemi tamamlandı

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -2,6 +2,7 @@
 using Business.BusinessAspects.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
@@ -25,13 +26,14 @@ namespace Business.Concrete
         public ProductManager(IProductDal productDal)
         {
             _productDal = productDal;
-        }
+        }           
         [SecuredOperation("product.add,Admin")]
         [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
         {
            var Toplam= _productDal.GetAll(p => p.CategoryId == product.CategoryId).Count;
-            if (Toplam >= 5)
+            if (Toplam >= 15)
             {
                 return new ErrorResult();
             }
@@ -39,6 +41,8 @@ namespace Business.Concrete
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
+        [CacheRemoveAspect("IProductService.Get")]
+
         public IResult Update(Product product)
         {
             if(product.ProductName.Length < 2)
@@ -49,6 +53,7 @@ namespace Business.Concrete
             return new SuccessResult(Messages.ProductUpdated);
         }
 
+        [CacheAspect]
         public IDataResult<List<Product>> GetAll()
         {
             if (DateTime.Now.Hour==22)
@@ -69,6 +74,7 @@ namespace Business.Concrete
 
         }
 
+        [CacheAspect]
         public IDataResult<Product> GetById(int productId)
         {
             return new SuccessDataResult<Product>(_productDal.Get   (p=>p.ProductId==productId),Messages.ProductDetailListed);
@@ -78,7 +84,7 @@ namespace Business.Concrete
         {
             return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails(),Messages.ProductsListed);
         }
-
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Delete(Product product)
         {
             _productDal.Delete(product);
